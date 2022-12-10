4da53c75465aa630131427fc12435d0000641fda
Max Borghino <fmborghino@gmail.com>
2022-12-09 22:50:06 -0800

    Day10 with common scanning bits factored out, and novel bits passed in a function parameter (woo!)

--- src/Day10.kt
+++ src/Day10.kt
@@ -1,12 +1,12 @@
 fun main() {
+    @Suppress("unused")
     fun log(message: Any?) {
         println(message)
     }
 
-    fun part1(input: List<String>): Int {
+    fun scanAndDoStuff(input: List<String>, doStuff: (Int, Int) -> Unit) {
         var cycle = 1
         var x = 1
-        var result = 0
         // shove an extra noop before every addx to make the cycle counts simpler
         input.flatMap {
             if (it == "noop") listOf(it) else listOf("noop", it)
@@ -20,6 +20,13 @@ fun main() {
                     cycle++
                 }
             }
+            doStuff(cycle, x)
+        }
+    }
+
+    fun part1(input: List<String>): Int {
+        var result = 0
+        scanAndDoStuff(input) { cycle, x ->
             if (cycle == 20 || (cycle - 20) % 40 == 0) {
                 val signalStrength = cycle * x
                 result += signalStrength
@@ -30,23 +37,9 @@ fun main() {
     }
 
     fun part2(input: List<String>): String {
-        var cycle = 1
-        var x = 1
         val crt = MutableList(241) { ' ' }
 
-        // shove an extra noop before every addx to make the cycle counts simpler
-        input.flatMap {
-            if (it == "noop") listOf(it) else listOf("noop", it)
-        }.forEach { line ->
-            log("  $cycle -> $line -> $x")
-            when {
-                line.startsWith("noop") -> cycle++
-                line.startsWith("addx") -> {
-                    val value = line.split(" ")[1].toInt()
-                    x += value
-                    cycle++
-                }
-            }
+        scanAndDoStuff(input) { cycle, x ->
             val offsetX = (cycle - 1) % 40
             crt[cycle - 1] = if (x in offsetX -1 .. offsetX + 1) '#' else ' '
         }
