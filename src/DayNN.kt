bf0390b4121366c9a2bac5c64644b095ed98086d
Max Borghino <fmborghino@gmail.com>
2022-12-16 19:40:02 -0800

    Template tweaks

--- src/DayNN.kt
+++ src/DayNN.kt
@@ -1,26 +1,37 @@
 fun main() {
-    @Suppress("unused")
+    val _day_ = 16
     fun log(message: Any?) {
         println(message)
     }
 
     fun part1(input: List<String>): Int {
-        return input.size
+        return -1
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return -2
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("DayNN_test.txt")
-    check(part1(testInput) == 99)
-//    check(part2(testInput) == 999)
+    // test inputs
+    val testInput = readInput("Day${_day_}_test.txt")
 
+    // test part 1
+    val test1 = part1(testInput)
+    check(test1 == 111) { "!!! test part 1 failed with: $test1" }
+    // test part 2
+    val test2 = part2(testInput)
+    check(part2(testInput) == 222) { "!!! test part 2 failed with: $test2" }
 
-//    val input = readInput("DayNN.txt")
-//    println(part1(input))
-//    check(part1(input) == 9999)
-//    println(part2(input))
-//    check(part2(input) == 99999)
+    // game inputs
+    val gameInput = readInput("Day${_day_}.txt")
+
+    // game part 1
+    val game1 = part1(gameInput)
+    println("*** game part 1: $game1")
+    check(game1 == 111111)
+
+    // game part 2
+    val game2 = part2(gameInput)
+    println("*** game part 2: $game2")
+    check(game2 == 222222)
 }
